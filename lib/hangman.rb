fdd523b72e327ac459d2f1c211a2a8b56d73bf05
Ashley Laswell <lswl9898@gmail.com>
2020-11-08 22:06:49 -0500

    add option to solve

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,7 +4,7 @@ correct_length_dictionary = Array.new
 
 def correct_length(correct_length_dictionary, words)
 	if words.length >= 5 and words.length <= 12
-		correct_length_dictionary.push(words)
+		correct_length_dictionary.push(words.downcase)
 	else
 	end
 end
@@ -27,3 +27,10 @@ puts random_word_length
 guess_array = Array.new(random_word_length, "_")
 
 print guess_array
+puts "\n"
+
+puts "Guess a letter If you want to solve press 1."
+get_guess = gets.chomp
+if get_guess == "1"
+	puts "What do you think the word is?"
+end
